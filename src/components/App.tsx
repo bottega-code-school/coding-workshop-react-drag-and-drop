63bf838986d3a7700b7e8e382b04325b303e3c6f
Jordan Hudgens <jordan@devcamp.com>
2021-08-04 13:25:32 -0700

    Built out initial data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,8 @@
 import * as React from "react";
-import HorizontalTree from "./HorizontalTree";
 import Layout from "./Layout";
 
 const App = () => {
-  return (
-    <Layout>
-      <HorizontalTree width={1200} height={400} />
-    </Layout>
-  );
+  return <Layout>PLACEHOLDER</Layout>;
 };
 
 export default App;
